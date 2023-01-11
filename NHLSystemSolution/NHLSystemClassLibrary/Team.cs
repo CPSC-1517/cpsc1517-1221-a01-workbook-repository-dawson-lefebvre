2d6e61db77f5b1d6421825f4dd5610c766e35b85
dawson-lefebvre <112987023+dawson-lefebvre@users.noreply.github.com>
2023-01-11 14:48:15 -0700

    started work on NHL team system

---
+++ NHLSystemSolution/NHLSystemClassLibrary/Team.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHLSystemClassLibrary
+{
+    internal class Team
+    {
+        Conference _conference;
+        Division _division;
+        string _name;
+        string _city;
+        string _arena;
+
+        public Conference Conference
+        {
+            get
+            {
+                return _conference;
+            }
+            private set
+            {
+                _conference = value;
+            }
+        }
+
+        public Division Division
+        {
+            get
+            {
+                return _division;
+            }
+            set
+            {
+                _division = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.All(char.IsLetter))
+                {
+
+                }
+            }
+        }
+    }
+}
