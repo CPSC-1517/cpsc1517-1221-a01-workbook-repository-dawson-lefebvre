688ff0019af17c478a649e11aec7a7c0fa0945b5
dawson-lefebvre <112987023+dawson-lefebvre@users.noreply.github.com>
2023-01-12 10:14:50 -0700

    finished fully implemented and changed Conference and Div to auto implemented

--- NHLSystemSolution/NHLSystemClassLibrary/Team.cs
+++ NHLSystemSolution/NHLSystemClassLibrary/Team.cs
@@ -8,35 +8,13 @@ namespace NHLSystemClassLibrary
 {
     internal class Team
     {
-        Conference _conference;
-        Division _division;
         string _name;
         string _city;
         string _arena;
 
-        public Conference Conference
-        {
-            get
-            {
-                return _conference;
-            }
-            private set
-            {
-                _conference = value;
-            }
-        }
+        public Conference Conference { get; set; }
 
-        public Division Division
-        {
-            get
-            {
-                return _division;
-            }
-            set
-            {
-                _division = value;
-            }
-        }
+        public Division Division { get; set; }
 
         public string Name
         {
@@ -48,9 +26,60 @@ namespace NHLSystemClassLibrary
             {
                 if (value.All(char.IsLetter))
                 {
-
+                    _name = value;
+                }
+                else
+                {
+                    throw new Exception("Name muts only contain letters");
                 }
             }
+        }
+
+        public string City
+        {
+            get
+            {
+                return _city;
+            }
+            set
+            {
+                if(value.Length >= 3)
+                {
+                    _city = value;
+                }
+                else
+                {
+                    throw new Exception("City must be longer than 2 characters");
+                }
+            }
+        }
+
+        public string Arena
+        {
+            get
+            {
+                return _arena;
+            }
+            set
+            {
+                if(value == null)
+                {
+                    throw new Exception("Cannot be blank");
+                }
+                else
+                {
+                    _arena = value;
+                }
+            }
+        }
+
+        public Team(Conference conference, Division division, string name, string city, string arena)
+        {
+            Conference = conference;
+            Division = division;
+            Name = name;
+            City = city;
+            Arena = arena;
         }
     }
 }
