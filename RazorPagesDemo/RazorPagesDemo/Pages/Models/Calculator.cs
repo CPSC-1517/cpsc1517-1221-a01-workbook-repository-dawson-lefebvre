dd6a696b92da411df15539885878c5d33563dc00
dawson-lefebvre <112987023+dawson-lefebvre@users.noreply.github.com>
2023-02-16 11:41:33 -0700

    m

---
+++ RazorPagesDemo/RazorPagesDemo/Pages/Models/Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace RazorPagesDemo.Pages.Models
+{
+    static class Calculator
+    {
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ RazorPagesDemo/RazorPagesDemo/Pages/SimpleCalculator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesDemo.Pages
+{
+    public class SimpleCalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
